0cffc542dcc9b1b0fec2afc0daf3ad0d04f5ad92
YunHyeRim1 <0305_08@naver.com>
2023-04-05 23:14:53 +0900

    JDBC, 스프링 통합 테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+import javax.sql.DataSource;
+
+@Configuration // 스프링 빈으로 관리된다
 public class SpringConfig {
+
+    private DataSource dataSource; // 스프링이 제공해준다(스프링부트가 application.properties 보고 스프링이 자체적으로 빈 생성, 데이터베이스와 연결할 수 있는 dataSource를 만들어준다)
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) { // 만들어준 dataSource를 주입 (DI)
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // 인터페이스는 new 안 되므로 구현체 사용
+//        return new MemoryMemberRepository(); // 인터페이스는 new 안 되므로 구현체 사용
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -3,32 +3,22 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest // 스프링 컨테이너와 테스트를 함께 실행한다. (스프링을 띄움)
+@Transactional // 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후에 항상 롤백한다.
+class MemberServiceIntegrationTest { // 통합 테스트
 
-class MemberServiceTest { // 단위 테스트
-
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    // 직접 new 하지 않고 MemberService 입장에서 보면 외부에서 memberRepository를 넣어줌 = DI(Dependency Injection)
-    @BeforeEach
-    public void beforeEach() { // 각 테스트를 실행하기 전에
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
@@ -58,24 +48,7 @@ class MemberServiceTest { // 단위 테스트
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-/*
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
-*/
 
-        //then
     }
 
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
