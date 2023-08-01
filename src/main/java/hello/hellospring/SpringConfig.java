a3d4eb16f8bbe6e414a8d23fc9c28da588a71d7e
YunHyeRim1 <0305_08@naver.com>
2023-08-01 23:05:26 +0900

    AOP

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +13,19 @@ import javax.sql.DataSource;
 @Configuration // 스프링 빈으로 관리된다
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+    //    private EntityManager em;
+
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    private DataSource dataSource; // 스프링이 제공해준다(스프링부트가 application.properties 보고 스프링이 자체적으로 빈 생성, 데이터베이스와 연결할 수 있는 dataSource를 만들어준다)
 //
@@ -28,15 +36,20 @@ public class SpringConfig {
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
+    /* @Component 쓰거나 아래처럼 쓰거나
     @Bean
-    public MemberRepository memberRepository() {
+    public TimeTraceAop timeTraceAop() {
+        return new TimeTraceAop();
+    }
+    */
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository(); // 인터페이스는 new 안 되므로 구현체 사용
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 
 }
